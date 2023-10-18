9aebe3cb3a6f61caa068176e94e0a7341b7109ce
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2023-10-18 12:21:07 -0300

    resolved PR issues. - fixed unclear imports - removed redundant files.

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from responses import *
+import os
+import logging
+import telegram
+from responses import (
+    start, handle_message
+)
+from telegram import Update
+from telegram.ext import ApplicationBuilder
 from telegram.ext import filters
 from telegram.ext import CommandHandler, MessageHandler
 
+# Start Write-Ahead Logs (For app status and debugging)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Start the telegram bot
+bot = telegram.Bot(os.getenv("BOT_TOKEN"))
+
+# Initialize Updater and Dispatcher
+PORT = int(os.environ.get('PORT', 5000))
+app = ApplicationBuilder().token(os.getenv('BOT_TOKEN')).build()
+
+# add a message handler
 msg_handler = MessageHandler(filters.ChatType.GROUPS & filters.ALL, handle_message)
 
+# main app
 def main(deploy=False):
 
     app.add_handler(CommandHandler("start", start, filters=~filters.ChatType.GROUPS)) #add specific handler for '/start
