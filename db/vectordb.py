d76b1f2a853a78935105ce7a7b845fd8f4013a67
makoohara <97365398+makoohara@users.noreply.github.com>
2023-11-04 17:08:31 +0800

    Revert "edited based on Josh's comment"
    This reverts commit b108a8fee9afa724f365d80ef2b2712dc614c91b.

--- db/vectordb.py
+++ db/vectordb.py
@@ -1,13 +1,13 @@
 import pinecone
-from typing import Sequence, cast
+from typing import Sequence, cast, List
 from dotenv import load_dotenv
 import os
 import sys
 from pathlib import Path
-from math import ceil
 
 sys.path.append(str(Path(__file__).parent.parent.absolute()))
 from db.db_types import PCEmbeddingData, PCQueryResults
+
 
 load_dotenv()
 
@@ -18,13 +18,11 @@ pinecone.init(
 embedding_index: pinecone.Index = pinecone.Index(pinecone.list_indexes()[0])
 
 
-def batch_upload_vectors(
-    index: pinecone.Index, all_embeddings: Sequence[PCEmbeddingData]
-) -> None:
+def batch_upload_vectors(index: pinecone.Index, all_embeddings: Sequence[PCEmbeddingData]) -> None:
     """
-    Takes in a very large list of embedding data, breaks this into batches of maximum size 100
+    Takes in a very large list of embedding data, breaks this into batches of maximum size 100 
     and calls the upload_vectors function on each batch.
-
+    
     Parameters
     ----------
     index : pinecone.Index
@@ -32,16 +30,18 @@ def batch_upload_vectors(
     all_embeddings : Sequence[PCEmbeddingData]
             The list of all message embeddings to upload to the Pinecone index.
     """
-
+    
     # Define the batch size
     BATCH_SIZE = 100
-
-    # Calculate the number of batches using ceil for more concise and readable code
-    num_batches = ceil(len(all_embeddings) / BATCH_SIZE)
-
+    
+    # Calculate the number of batches
+    num_batches = len(all_embeddings) // BATCH_SIZE + (1 if len(all_embeddings) % BATCH_SIZE != 0 else 0)
+    
     # Loop through each batch and upload
     for i in range(num_batches):
-        batch = all_embeddings[i * BATCH_SIZE : (i + 1) * BATCH_SIZE]
+        start_idx = i * BATCH_SIZE
+        end_idx = start_idx + BATCH_SIZE
+        batch = all_embeddings[start_idx:end_idx]
         upload_vectors(index, batch)
 
 
@@ -55,7 +55,7 @@ def upload_vectors(
     ----------
     index : pinecone.Index
                 The Pinecone index to upload the vectors to.
-    message_embeddings : Sequence[PCEmbeddingData]
+        message_embeddings : Sequence[PCEmbeddingData]
                 The list of message embeddings to upload to the Pinecone index. Each element is of the form
                 {"id": <string>, "values": <list[float]>, "metadata": <dict[str, Any]>}.
     """
