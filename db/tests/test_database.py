554ced31cffc8fab7a8cdc5902c580b05ed89392
MajaOstrowska <majaostrowska@uni.minerva.edu>
2023-11-16 17:00:20 -0300

    added endpoint for storing multiple msgs

--- db/tests/test_database.py
+++ db/tests/test_database.py
@@ -1,10 +1,9 @@
 import unittest
 from datetime import datetime
 from unittest.mock import MagicMock, patch
-
 from telegram import Chat, Message, User
-
-from db.database import store_message_to_db
+from typing import List
+from db.database import store_multiple_messages_to_db, store_message_to_db
 from db.db_types import AddMessageResult, SerializedMessage
 
 
@@ -71,3 +70,45 @@ class TestDatabase(unittest.TestCase):
 
         result = store_message_to_db(self.chat_id, self.serialized_message)
         self.assertEqual(result, AddMessageResult.EXISTING)
+
+
+class TestStoreMessagesToDB(unittest.TestCase):
+    def setUp(self) -> None:
+        self.chat_id = 12345
+        self.user = User(id=123, first_name="Test", is_bot=False)
+        self.chat = Chat(id=self.chat_id, type="group", title="Test Group")
+        self.message_1 = Message(
+            message_id=1,
+            date=datetime.now(),
+            from_user=self.user,
+            chat=self.chat,
+            text="Test message 1",
+        )
+        self.message_2 = Message(
+            message_id=2,
+            date=datetime.now(),
+            from_user=self.user,
+            chat=self.chat,
+            text="Test message 2",
+        )
+        self.serialized_messages = [
+            SerializedMessage(self.message_1),
+            SerializedMessage(self.message_2),
+        ]
+
+    @patch("db.database.db")
+    def test_store_messages_to_db(self, mock_db: MagicMock) -> None:
+        mock_db.active_groups.find_one.side_effect = [None, None]
+        mock_db.active_groups.insert_one.return_value = MagicMock()
+        mock_db.active_groups.update_one.return_value = MagicMock(
+            acknowledged=True, matched_count=1, modified_count=1
+        )
+
+        result = store_multiple_messages_to_db(self.chat_id, self.serialized_messages)
+        self.assertEqual(result, AddMessageResult.SUCCESS)
+
+        # Add assertions for any additional checks you want to make
+
+
+if __name__ == "__main__":
+    unittest.main()
