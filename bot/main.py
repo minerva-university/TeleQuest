83fde8bfaddc49d0939c8918ffdc8c4d918833b6
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2023-11-30 01:01:42 -0300

    restructure bot subdirectory

--- bot/main.py
+++ bot/main.py
@@ -1,30 +1,9 @@
 import os
-import sys
-from pathlib import Path
-
-BASE_DIR = os.path.join(Path(__file__).parent.parent)
-sys.path.append(BASE_DIR)
-import logging
-import telegram
 from bot.responses import start, help, handle_message
-from telegram.ext import ApplicationBuilder
 from telegram.ext import filters
 from telegram.ext import CommandHandler, MessageHandler
+from . import app, PORT, BOT_TOKEN
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-# Start Write-Ahead Logs (For app status and debugging)
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-# Start the telegram bot
-bot = telegram.Bot(BOT_TOKEN)
-
-# Initialize Updater and Dispatcher
-PORT = int(os.environ.get("PORT", 5000))
-app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 # add a message handler
 msg_handler = MessageHandler(filters.ChatType.GROUPS & filters.ALL, handle_message)
@@ -49,8 +28,3 @@ def main(deploy: bool = False) -> None:
         app.run_polling()  # this is used to run the bot locally
 
     app.idle()  # type: ignore
-
-
-if __name__ == "__main__":
-    deploy = "--deploy" in sys.argv or "-d" in sys.argv
-    main(deploy=deploy)
