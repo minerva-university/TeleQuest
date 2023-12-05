a2b81cb2e3a771cce1d6d13360ebddbdbc99d7e1
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2023-12-05 12:32:03 -0300

    switched bot execution order

--- bot/main.py
+++ bot/main.py
@@ -41,7 +41,9 @@ def main(
     app.add_handler(CommandHandler("help", help, filters=~filters.ChatType.GROUPS))
     app.add_handler(msg_handler)  # add message handler
 
-    if deploy:
+    if not deploy:
+        app.run_polling()  # this is used to run the bot locally
+    else:
         # listens for requests from any addresses
         app.run_webhook(
             listen="0.0.0.0",
@@ -49,8 +51,5 @@ def main(
             url_path=BOT_TOKEN,
             webhook_url=f"https://tele-quest-ecdd62e1d0d3.herokuapp.com/{os.getenv('BOT_TOKEN')}",
         )
-
-    else:
-        app.run_polling()  # this is used to run the bot locally
 
     app.idle()  # type: ignore
