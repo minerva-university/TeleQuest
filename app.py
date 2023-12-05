686f08ef22de83d11d8cde4e8736b7d65eb25c66
Joshua <joshjohnmark@gmail.com>
2023-12-05 11:18:54 -0300

    added a function to initialise the bot, then run based on deploy flag

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sys
-from bot.main import main
+from bot.main import main, init
 
 
 if __name__ == "__main__":
     deploy = "--deploy" in sys.argv or "-d" in sys.argv
-    main(deploy=deploy)
+    app, msg_handler, PORT, BOT_TOKEN = init(deploy=deploy)
+    main(app, msg_handler, PORT, BOT_TOKEN, deploy=deploy)
