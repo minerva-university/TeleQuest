bf1abc2d8387d726114dacb274987c13feb23c1e
Joshua <joshjohnmark@gmail.com>
2023-12-05 12:05:27 -0300

    added function to initialise the bot for local testing and deployment

--- bot/responses.py
+++ bot/responses.py
@@ -75,7 +75,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     context: telegram.ext.Context
         The context object that is received from the telegram bot.
     """
-    print("here")
     effective_chat = update.effective_chat
 
     # get the id of the group chat
@@ -83,7 +82,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
     # get the message object
     msg = update.message
-    print(msg)
 
     if not (msg and chat_id):
         return
@@ -107,7 +105,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         elif command.startswith("/q"):
             parts = msg.text.split("/q")
             question = max(parts, key=len).strip()
-            print(question)
 
             if len(question) > MIN_QUESTION_LENGTH:
                 await respond_to_question(question, chat_id, msg.message_id, context)
@@ -127,7 +124,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         # TODO: Logic to handle different types of messages
         message: SerializedMessage | None = SerializedMessage(msg) if msg else None
         store_message = message and store_message_to_db(chat_id, message)
-        print(store_message)
         store_message_success = store_message == AddMessageResult.SUCCESS
         if not store_message_success:
             return
